519cc54707daf45d08e301c71cf7f86035df8a68
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-05-08 13:47:49 +0900

    Modify 목록화면 grid로 layout 구성

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -7,10 +7,10 @@
 function Projects() {
 
     return (
-        <div className="overflow-hidden" style={{'background': '#ccc'}}>
+        <div className="overflow-hidden container" style={{'background': '#ccc'}}>
             {/*<div className="header relative z-10 mb-20 px-4 py-8 ring-1 ring-zinc-900" style={{'background': '#ff0'}}>*/}
-            <div className="header relative z-10 mb-8 px-4 py-8 ring-1 ring-zinc-900" style={{'background': '#fff'}}>
-                <div className="mx-auto lg:max-w-7xl flex items-center justify-between">
+            <div className="header relative z-10 mb-8 py-8 ring-1 ring-zinc-900" style={{'background': '#fff'}}>
+                <div className="container mx-auto lg:max-w-7xl flex items-center justify-between">
                     <div className="flex items-center gap-2 sm:gap-4">
                         <span className="zinc-ring ring-zinc-900 hidden sm:inline icon-home"><img
                             src="/assets/logo-3.svg" alt="logo3"/></span>
@@ -25,8 +25,8 @@ function Projects() {
                     </div>
                 </div>
             </div>
-            <section className="content px-4">
-                <div className="mx-auto lg:max-w-7xl">
+            <section className=" px-4">
+                <div className="container mx-auto lg:max-w-7xl">
                     <div className="content--title">Projects</div>
                     {/*<ul className="columns-3xs gap-8">*/}
                     {/*    <li style={{'background-color': 'green'}} className="content--item aspect-square">1</li>*/}
@@ -40,17 +40,30 @@ function Projects() {
                     {/*    <li style={{'background-color': 'green'}} className="content--item mt-8 aspect-square">9</li>*/}
                     {/*    <li style={{'background-color': 'green'}} className="content--item mt-8 aspect-video">10</li>*/}
                     {/*</ul>*/}
-                    <ul className="flex flex-wrap -mx-8">
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-square">1</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-video">2</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-square">3</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-video">4</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-square">5</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-video">6</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-square">7</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-video">8</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-square">9</li>
-                        <li style={{'background-color': 'green'}} className="content--item w-full sm:w-1/3 md:w-1/5 aspect-video">10</li>
+                    {/* flex를 사용하는 경우 tailwind config 값을 수정하여서 gap-width 값을 작성해야 한다. */}
+                    {/*<ul className="flex flex-wrap -m-2">*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-square">1</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-video">2</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-square">3</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-video">4</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-square">5</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-video">6</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-square">7</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-video">8</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-square">9</li>*/}
+                    {/*    <li style={{'background-color': 'green'}} className="content--item m-2 w-full sm:w-1/3 md:w-1/5 aspect-video">10</li>*/}
+                    {/*</ul>*/}
+                    <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 gap-4 items-center">
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">1</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">2</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">3</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">4</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">5</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">6</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">7</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">8</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">9</li>
+                        <li style={{'background-color': 'green'}} className="content--item aspect-square">10</li>
                     </ul>
                 </div>
             </section>
