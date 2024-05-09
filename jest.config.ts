de962654e6c304333d6825944cd37540173a98bc
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-05-09 17:55:06 +0900

    페이지네이션 기능 작성중

--- jest.config.ts
+++ jest.config.ts
@@ -12,17 +12,6 @@
 // };
 
 // 2번 테스트
-// export default {
-//     preset: 'ts-jest',
-//     testEnvironment: 'jest-environment-jsdom',
-//     setupFilesAfterEnv: ['./jest.setup.ts'],
-//     moduleNameMapper: {
-//         '^.+\\.svg$': 'jest-svg-transformer',
-//         '\\.(css|less|scss)$': 'identity-obj-proxy',
-//     },
-// }
-
-// 2번의  변형
 export default {
     preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
@@ -31,8 +20,21 @@ export default {
         '^.+\\.svg$': 'jest-svg-transformer',
         '\\.(css|less|scss)$': 'identity-obj-proxy',
     },
-    // A list of paths to directories that Jest should use to search for files in
     roots: ['<rootDir>'],
-    // A list of paths to modules that run some code to configure or set up the testing framework before each test
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
+
+// 2번의  변형
+// export default {
+//     preset: 'ts-jest',
+//     testEnvironment: 'jest-environment-jsdom',
+//     moduleNameMapper: {
+//         '@/(.*)': '<rootDir>/src/$1',
+//         '^.+\\.svg$': 'jest-svg-transformer',
+//         '\\.(css|less|scss)$': 'identity-obj-proxy',
+//     },
+//     // A list of paths to directories that Jest should use to search for files in
+//     roots: ['<rootDir>'],
+//     // A list of paths to modules that run some code to configure or set up the testing framework before each test
+//     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+// }
--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,8 +1,8 @@
 // !!TODO: db값을 object로 반환하는 함수 별도 생성 -> build 명령어시 에러 발생, 파일 색제
 // !!TODO: 스크린샷을 참고하여 레이아웃 생성
 // !!TODO: db값이랑 바인딩
-// TODO: 모바일 화면 대응
-// TODO: 테스트 코드 작성
+// !!TODO: 모바일 화면 대응
+// !!TODO: 테스트 코드 작성
 // TODO: 페이지네이션 추가
 // TODO: 아이템 수정 기능 추가
 // TODO: 목록 로딩, 목록 에러 상태 추가.
@@ -11,60 +11,14 @@ import {Card} from "@/pages/projects/Card.tsx";
 import projects from "@/api/ProjectAPI.ts";
 
 function Projects() {
-    // const mock = [
-    //     {
-    //         "id": "1",
-    //         "name": "Johnson - Kutch",
-    //         "description": "Fully-configurable intermediate framework. Ullam occaecati libero laudantium nihil voluptas omnis qui modi qui.",
-    //         "imageUrl": "/assets/placeimg_500_300_arch4.jpg",
-    //         "contractTypeId": 3,
-    //         "contractSignedOn": "2013-08-04T22:39:41.473Z",
-    //         "budget": 54637,
-    //         "isActive": false
-    //     },
-    //     {
-    //         "id": "2",
-    //         "name": "Dillesik LLCs",
-    //         "description": "Re-contextualized dynamic moratorium. Aut nulla soluta numquam qui dolor architecto et facere dolores.",
-    //         "imageUrl": "/assets/placeimg_500_300_arch12.jpg",
-    //         "contractTypeId": 6,
-    //         "contractSignedOn": "2016-06-26T18:24:01.706Z",
-    //         "budget": 29729,
-    //         "isActive": true
-    //     },
-    //     {
-    //         "id": "3",
-    //         "name": "Purdy, Keeling and Smithams",
-    //         "description": "Innovative 6th generation model. Perferendis libero qui iusto et ullam cum sint molestias vel.",
-    //         "imageUrl": "/assets/placeimg_500_300_arch5.jpg",
-    //         "contractTypeId": 4,
-    //         "contractSignedOn": "2013-05-26T01:10:42.344Z",
-    //         "budget": 45660,
-    //         "isActive": true
-    //     },
-    //     {
-    //         "id": "4",
-    //         "name": "Kreiger - Waelchis",
-    //         "description": "Managed logistical migration. Qui quod praesentium accusamus eos hic non error modi et.",
-    //         "imageUrl": "/assets/placeimg_500_300_arch12.jpg",
-    //         "contractTypeId": 2,
-    //         "contractSignedOn": "2009-12-18T21:46:47.944Z",
-    //         "budget": 81188,
-    //         "isActive": true
-    //     },
-    //     {
-    //         "id": "5",
-    //         "name": "Crona Inc",
-    //         "description": "Monitored explicit methodology. Rem quos maxime amet autem beatae quisquam excepturi sint velit.",
-    //         "imageUrl": "/assets/placeimg_500_300_arch5.jpg",
-    //         "contractTypeId": 1,
-    //         "contractSignedOn": "2016-11-14T19:19:53.624Z",
-    //         "budget": 31350,
-    //         "isActive": true
-    //     }
-    // ]
     const mock = projects();
     console.log('mock', mock);
+    // mock의 아이템을 페이지네이션의 정보에 따라서 다르게 펼치기.
+    // useEffect를 이용하여서 페이지 값이 변경되면 다르게 화면 노출하기.
+    // 페이지네이션에 필요한 state값은 json.server의 param 값을 참조. (page, perPage)
+    // 예외조건  : 페이지는 0보다 작아질수 없음
+    // 예외조건 : 페이지는 마지막 페이지보다 커질수 없음
+    // state: page, isLoading, isError
 
     const mockList = mock.map((item) => (
         <Card key={item.id} project={item}></Card>
