568667d38638fc90dd228f1ce52f82ee2c9a511d
hwigyeom.kim <hwigyeom.kim@dkbmc.com>
2024-05-09 15:23:02 +0900

    Jest 설정작업 완료

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,23 @@
+// 1번 테스트
+// export default {
+//     testEnvironment: "jsdom",
+//     transform: {
+//         "^.+\\.tsx?$": "ts-jest",
+//     },
+//     moduleNameMapper: {
+//         "^.+\\.svg$": "jest-svg-transformer",
+//         "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+//     },
+//     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+// };
+
+// 2번 테스트
 export default {
-    testEnvironment: "jsdom",
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
-    },
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['./jest.setup.ts'],
     moduleNameMapper: {
-        "^.+\\.svg$": "jest-svg-transformer",
-        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        '^.+\\.svg$': 'jest-svg-transformer',
+        '\\.(css|less|scss)$': 'identity-obj-proxy',
     },
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
+}
